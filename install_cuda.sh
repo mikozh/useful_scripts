9b4d00d45b4a48c6634dde7a3508c7d0deda4793
Mikhail Ozhgibesov <omiser@gmail.com>
2026-02-12 06:32:57 +0100

    Add installation of NVIDIA driver in script

--- install_cuda.sh
+++ install_cuda.sh
@@ -1,3 +1,5 @@
+sudo apt install nvidia-driver-570-server
+
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
 
