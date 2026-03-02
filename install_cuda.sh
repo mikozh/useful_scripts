266ed3013ec4cdd80d59da1d80605be6a7ec4351
Mikhail Ozhgibesov <omiser@gmail.com>
2026-03-02 13:35:46 +0700

    Update install_cuda.sh

--- install_cuda.sh
+++ install_cuda.sh
@@ -8,4 +8,4 @@ wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
 
 sudo apt update
-sudo apt install -y cuda-toolkit cuda-drivers
+sudo apt install -y cuda-toolkit
