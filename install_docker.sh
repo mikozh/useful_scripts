48d17f632c92dd3e4b6e7938c09c081ba32ccd58
Mikhail Ozhgibesov <omiser@gmail.com>
2026-03-02 22:50:42 +0700

    Create install_docker.sh

---
+++ install_docker.sh
@@ -0,0 +1,14 @@
+# Remove old versions
+apt remove docker docker-engine docker.io containerd runc
+
+# Install prerequisites
+apt update && upgrade -y
+apt install -y ca-certificates curl gnupg lsb-release
+
+# Add Docker GPG key + repo
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt update
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
